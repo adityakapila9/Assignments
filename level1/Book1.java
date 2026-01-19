74ade4772d4f3f1c967a50c0a8a6eb48024b9c2f
adityakapila9 <adityakapila9@gmail.com>
2026-01-19 10:12:40 +0530

    new

--- level1/Book1.java
+++ level1/Book1.java
@@ -1,17 +1,17 @@
 package Level1_16_01_2026;
 
-public class Book {
+public class Book1 {
     private String title;
     private String author ;
     private int price;
 
-    Book()
+    Book1()
     {
         this.title="Atomic Habits";
         this.author="James Clear";
         this.price=300;
     }
-    Book(String title,String author,int price)
+    Book1(String title,String author,int price)
     {
         this.title=title;
         this.author=author;
--- level1/Circle1.java
+++ level1/Circle1.java
@@ -1,12 +1,12 @@
 package Level1_16_01_2026;
 
-public class Circle {
+public class Circle1 {
     private int radius;
-    Circle()
+    Circle1()
     {
         this.radius=5;
     }
-    Circle(int radius)
+    Circle1(int radius)
     {
         this.radius=radius;
     }
